27741da6f4d654c782b19c29323b615583597b09
deivissonss <deivisson.santos@detran.ba.gov.br>
2023-03-28 18:50:47 -0300

    [FIX] Ajustes nas libs para compilação

--- WebAPI-My-Home-Library/obj/Debug/net5.0/WebAPI-My-Home-Library.MvcApplicationPartsAssemblyInfo.cs
+++ WebAPI-My-Home-Library/obj/Debug/net5.0/WebAPI-My-Home-Library.MvcApplicationPartsAssemblyInfo.cs
@@ -11,7 +11,6 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Community.Core.EnterpriseLibrary")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
